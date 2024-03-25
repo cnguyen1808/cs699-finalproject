88f8bb33028461234a3df825ecb57c60484dd7bd
Gadokmo <chuongng@bu.edu>
2024-03-24 22:14:54 -0400

    t push asdasdasdasdasd

---
+++ METCS699_finalproject_Bartol_Nguyen.R
@@ -0,0 +1,425 @@
+###############################################################################
+# MET CS699
+# Final Project
+# Brandon Bartol & Chuong Nguyen
+###############################################################################
+# install.packages("GGally")
+# install.packages("modeest")
+# install.packages("dplyr")
+# install.packages("caret")
+# install.packages("Boruta")
+# install.packages("ROSE")
+library(rsample)
+library(GGally)
+library(modeest)
+library(dplyr)
+library(caret)
+library(Boruta)
+library(ROSE)
+library(caret) 
+library(e1071)
+library(pROC)
+
+dir <- file.path("C:", "Users", "Admin", "iCloudDrive",
+                 "CS699 - Data Mining", "FinalProject", "git")
+setwd(dir)
+
+# setwd("C:\\Users\\brand\\OneDrive\\Documents\\699\\TermProject\\Data")
+
+Project_Data <- read.csv("project_dataset_5K.csv",header = TRUE)
+Project_Data
+
+Project_Data_Cleansed <- Project_Data
+sapply(Project_Data_Cleansed, class)
+
+###############################################################################
+# Part I: Data preprocessing
+###############################################################################
+
+#Count missing data in each column
+sapply(Project_Data_Cleansed, function(x) sum(is.na(x)))
+#remove columns with more than 90% of the data present
+Project_Data_Cleansed2 <- Project_Data_Cleansed %>%
+  select_if(function(x) sum(is.na(x)) < 1000) # INCREASED from 500 to keep 2 columns with 508 na values
+
+
+sapply(Project_Data_Cleansed2, function(x) sum(is.na(x)))
+
+
+#Add data to columns with null values:
+#if column contains <= 5 unique values, then the mode is applied
+#else the median is applied
+#this is stratified for the Class column
+grouped <- split(Project_Data_Cleansed2, Project_Data_Cleansed2$Class)
+
+for (col_name in colnames(Project_Data_Cleansed2)) {
+  unique_values <- unique(Project_Data_Cleansed2[[col_name]])
+  
+  if (length(unique_values) <= 5) {
+    # Compute mode value for each group
+    mode_value <- sapply(grouped, function(group) {
+      mfv(group[[col_name]][!is.na(group[[col_name]])])
+    })
+    Project_Data_Cleansed2[[col_name]] <- ifelse(is.na(Project_Data_Cleansed2[[col_name]]), mode_value[Project_Data_Cleansed2$Class], Project_Data_Cleansed2[[col_name]])
+  } else {
+    # Compute median value for each group
+    non_missing_values <- Project_Data_Cleansed2[[col_name]][!is.na(Project_Data_Cleansed2[[col_name]])]
+    median_value <- median(non_missing_values)
+    Project_Data_Cleansed2[[col_name]] <- ifelse(is.na(Project_Data_Cleansed2[[col_name]]), median_value, Project_Data_Cleansed2[[col_name]])
+  }
+}
+
+
+#find highly correlated columns to remove duplicate influence (corr > .7)
+corr <- cor(Project_Data_Cleansed2[,-ncol(Project_Data_Cleansed2)])
+findCorrelation(corr, cutoff = 0.7, names = TRUE)
+CorrColumnRemoved <- findCorrelation(corr, cutoff = 0.7, names = FALSE)
+Project_Data_Cleansed2 <- Project_Data_Cleansed2[, -CorrColumnRemoved]
+
+#removing columns with near zero variance, changed frequency to 20 to match class notes
+nearZeroVar(Project_Data_Cleansed2, freqCut = 20, saveMetrics =  TRUE)
+near_zero_vars <- nearZeroVar(Project_Data_Cleansed2, freqCut = 20, saveMetrics = FALSE)
+Project_Data_Cleansed2 <- Project_Data_Cleansed2[, -near_zero_vars]
+
+sapply(Project_Data_Cleansed2, class)
+
+
+###############################################################################
+# Part II: Apply feature selection and unbalanced-data treatment models
+###############################################################################
+
+# Split processed data set into training and testing data sets
+# This test data set will be used in the very final evaluation step
+splitting.df <- Project_Data_Cleansed2
+splitting.df$Class <- factor(splitting.df$Class, levels = c("N", "Y"))
+
+set.seed(31)
+outer.split <- initial_split(splitting.df, prop = 0.66, strata = Class)
+main.train <- training(outer.split)
+main.test <- testing(outer.split)
+
+Project_Data_Cleansed2 <- main.train
+
+###########################
+# Feature selection 1
+###########################
+
+#Use boruta to identify unimportant attributes
+Project_Data_Cleansed3 <- Project_Data_Cleansed2
+Project_Data_Cleansed3$Class <- factor(Project_Data_Cleansed2$Class, levels = c("N", "Y"))
+borutaResult <- Boruta(Class ~ ., data = Project_Data_Cleansed3)
+borutaResult$finalDecision
+
+#remove rejected columns
+BorutaRejectedColumns <- names(borutaResult$finalDecision[borutaResult$finalDecision == "Rejected"])
+Project_Data_Cleansed3 <- Project_Data_Cleansed3[,!(names(Project_Data_Cleansed3) %in% BorutaRejectedColumns)]
+
+
+#Used Code provided in Class to do PCA
+df <- Project_Data_Cleansed3[, -(1:9)]
+sapply(df, class)
+
+# split dataset
+set.seed(31)
+split <- initial_split(df, prop = 0.66, strata = Class)
+training <- training(split)
+test <- testing(split)
+
+
+###########################
+# Feature selection 2
+###########################
+
+# build Weka’s J48 decision tree model using all attributes
+library(RWeka)
+J48.model <- J48(Class ~ . , data=training)
+# test the model on the test dataset
+pred <- predict(J48.model, newdata = test, type = "class")
+performance_measures  <- confusionMatrix(data=pred,
+                                         reference = test$Class)
+performance_measures
+
+# apply PCA on the training dataset
+pc <- prcomp(training[, -ncol(training)] %>% 
+               select_if(~ all(is.numeric(.))), 
+             center = TRUE, scale = TRUE) # exclude class attribute
+summary(pc)
+#PCA finds 32 principal components, we can preserve 90% of the total variability.
+
+
+###########################
+# Feature selection 3 -Brandon
+###########################
+
+
+
+
+###############################################
+# Balance with Oversampling and Undersampling - Chuong
+###############################################
+
+balancing.df <- Project_Data_Cleansed2
+balancing.df$Class <- factor(balancing.df$Class)
+sapply(balancing.df, class)
+
+
+###################
+# (1) over-sampling 
+###################
+over_balance <- function(imbalanced.df, target_col) {
+  
+  imbalanced.df <<- imbalanced.df
+  
+  # Perform over-sampling + under-sampling
+  sampled_df <- ROSE::ovun.sample(Class ~ ., 
+                                  data = as.data.frame(imbalanced.df),
+                                  method = "over", 
+                                  p = 0.5,
+                                  seed = 31)$data
+  
+  return(sampled_df)
+}
+
+
+test.over.df <- over_balance(as.data.frame(balancing.df))
+table(test.over.df$Class)
+
+
+####################
+# (2) under-sampling 
+####################
+under_balance <- function(imbalanced.df, target_col) {
+  
+  imbalanced.df <<- imbalanced.df
+  
+  # Perform over-sampling + under-sampling
+  sampled_df <- ROSE::ovun.sample(Class ~ ., 
+                                  data = as.data.frame(imbalanced.df),
+                                  method = "under", 
+                                  p = 0.5,
+                                  seed = 31)$data
+  
+  return(sampled_df)
+}
+
+
+test.under.df <- under_balance(as.data.frame(balancing.df))
+table(test.under.df$Class)
+
+###########################
+# (3) over + under-sampling 
+###########################
+combine_sampling_balance <- function(imbalanced.df, target_col) {
+  
+  imbalanced.df <<- imbalanced.df
+  
+  # Perform over-sampling + under-sampling
+  sampled_df <- ROSE::ovun.sample(Class ~ ., 
+                                  data = as.data.frame(imbalanced.df),
+                                  method = "both", 
+                                  p = 0.5,
+                                  seed = 31,
+                                  N = nrow(imbalanced.df))$data
+  
+  return(sampled_df)
+}
+
+
+test.overunder.df <- combine_sampling_balance(as.data.frame(balancing.df))
+table(test.overunder.df$Class)
+
+
+##################################
+# (4) Bootstrap balancing - Chuong
+##################################
+# Function to perform bootstrap resampling to balance dataset
+bootstrap_balance <- function(df, target_col, balance_ratio) {
+  # Split dataset into majority and minority classes
+  majority_class <- df[df[[target_col]] == "N", ]
+  minority_class <- df[df[[target_col]] == "Y", ]
+  
+  # Calculate the number of samples needed from the minority class
+  minority_count <- nrow(minority_class)
+  majority_count <- nrow(majority_class)
+  target_minority_count <- balance_ratio * majority_count
+  
+  # Generate bootstrap samples from the minority class
+  balanced_data <- minority_class[sample(1:minority_count, 
+                                         target_minority_count, 
+                                         replace = TRUE), ]
+  
+  # Combine with majority class
+  balanced_data <- rbind(balanced_data, majority_class)
+  
+  return(balanced_data)
+}
+
+test.bootstrap.df <- bootstrap_balance(main.train, "Class", 1)
+table(test.bootstrap.df$Class)
+
+##############################################
+# Data visualizations -Chuong
+##############################################
+# Pairwise
+
+
+# Assuming 'dataset' is your dataframe
+
+
+
+###############################################################################
+# Part III: Model Building
+###############################################################################
+
+# Define a function to compute metrics and build the result table
+# The factor in Boruta is currently give No as Positive Class
+# While the goal of the project is identifying people with disorder "Y"
+
+
+compute_metrics_and_build_table <- function(predicted_classes, actual_labels) {
+  # Compute confusion matrix
+  cm <- confusionMatrix(data = predicted_classes, reference = actual_labels)
+  
+  TP <- as.numeric(cm$table[2,2])
+  TN <- as.numeric(cm$table[1,1])
+  FP <- as.numeric(cm$table[2,1])
+  FN <- as.numeric(cm$table[1,2])
+  
+  TPR = TP / (TP + FN)
+  FPR = FP / (FP + TN)
+  Precision = TP / (TP + FP)
+  Recall = TP / (TP + FN)
+  
+  MCCoef = ((TP*TN) - (FP*FN))/sqrt((TP+FP)*(TP+FN)*(TN+FP)*(TN+FN))
+
+  Po = (TP + TN) / sum(cm$table)
+  Pe1 = ((TP + FN) / sum(cm$table)) * ((TP + FP) / sum(cm$table))
+  Pe2 = ((FP + TN) / sum(cm$table)) * ((FN + TN) / sum(cm$table))
+  Pe = Pe1 + Pe2
+  
+  # Extract metrics for each class
+  class_metrics <- data.frame(
+    Class = rownames(cm$table),
+    TP_Rate = round(TPR, digits=3),
+    FP_Rate = round(FPR, digits=3),
+    Precision = round(Precision, digits=3),
+    Recall = round(Recall, digits=3),
+    F_Measure = round(2 * Precision * Recall / (Precision + Recall), digits=3),
+    MCC = round(MCCoef, digits=3),
+    Kappa = round((Po - Pe) / (1 - Pe), digits=3)
+  )
+  
+  # # Compute ROC area for each class
+  # ROC_Area <- sapply(predicted_classes, function(class_pred) {
+  #   roc_curve <- roc(ifelse(predicted_classes == class_pred, 1, 0), ifelse(actual_labels == class_pred, 1, 0))
+  #   auc(roc_curve)
+  # })
+  
+  # # Compute weighted average of each measure
+  # weighted_average <- colMeans(class_metrics[, -1])
+  # weighted_average["Class"] <- "Weighted Average"
+  
+  # Bind the class metrics together
+  result_table <- rbind(class_metrics) 
+                        # data.frame(Class = "ROC Area", Value = ROC_Area),
+                        # weighted_average)
+  
+  print(cm$table)
+  
+  # Return the result table
+  return(result_table)
+}
+
+
+##############################################
+# Split training and testing sets
+##############################################
+# split dataset
+set.seed(31)
+split <- initial_split(df, prop = 0.66, strata = Class)
+training <- training(split)
+test <- testing(split)
+
+balanced.training.df <- bootstrap_balance(training, "Class", 0.5)
+
+##############################################
+# Model 1: Naive Bayes -Chuong
+##############################################
+
+# Build Naive Bayes model from training set
+nb.mdl <-naiveBayes(Class ~ ., data = balanced.training.df)
+
+# Test the model on testing set
+nb.mdl.pred <- predict(nb.mdl, newdata = test, type = "class")
+nb.mdl.pred.cf <- compute_metrics_and_build_table(nb.mdl.pred, test$Class)
+nb.mdl.pred.cf
+
+# Test the model on original testing set
+nb.mdl.main.pred <- predict(nb.mdl, newdata = main.test, type = "class")
+nb.mdl.main.pred.cf <- compute_metrics_and_build_table(nb.mdl.main.pred, main.test$Class)
+nb.mdl.main.pred.cf
+
+
+##############################################
+# Model 2: Logistic Reg -Chuong
+##############################################
+# disable scientific notation
+options(scipen=999)
+
+logitModel <- glm(Class ~ ., data = balanced.training.df, family = "binomial") 
+
+summary(logitModel)
+
+# Predict probability on test set
+logitmdl.pred <- predict(logitModel, test, type = "response")
+# performance measures on the test dataset
+logitmdl.pred <- factor(ifelse(logitmdl.pred >= 0.5, "Y", "N"))
+
+# Test the model on testing set
+nb.mdl.pred.cf <- compute_metrics_and_build_table(logitmdl.pred, test$Class)
+nb.mdl.pred.cf
+
+
+# Predict probability on original testing set
+logitmdl.main.pred <- predict(logitModel, main.test, type = "response")
+# performance measures on the test dataset
+logitmdl.main.pred <- factor(ifelse(logitmdl.main.pred >= 0.5, "Y", "N"))
+logitmdl.main.pred
+
+# Test the model on testing set
+nb.mdl.pred.cf <- compute_metrics_and_build_table(logitmdl.main.pred, 
+                                                  main.test$Class)
+nb.mdl.pred.cf
+
+##############################################
+# Model 3: Decision tree -Chuong
+##############################################
+
+
+
+##############################################
+# Model 4: Random Forest -Brandon
+##############################################
+# Para Tuning
+
+
+##############################################
+# Model 5: KNN -Brandon
+##############################################
+# Para Tuning
+
+
+##############################################
+# Model 6: NN -Brandon
+##############################################
+
+
+
+###############################################################################
+# Part III: Model Evaluation & Interpretation
+###############################################################################
+
+
+
+
+
---
+++ README.txt
@@ -0,0 +1,38 @@
+Classification Modeling Project: Predicting Depressive Disorders
+Overview
+This project aims to build and evaluate multiple classification models to predict whether a person has experienced a depressive disorder using the Behavioral Risk Factor Surveillance System (BRFSS) Survey Data from 2020. The dataset consists of 5000 tuples with 276 attributes, where each tuple represents a person and the target variable 'Class' indicates whether the person has experienced a depressive disorder ('Y') or not ('N').
+
+Dataset
+The dataset 'project_dataset-5K.csv' has been preprocessed and provided for this project. However, further preprocessing is required, including handling missing values, cleaning, feature selection, and encoding categorical variables.
+
+Goal
+The main goal of this project is to:
+
+Perform preprocessing on the dataset as needed.
+Build multiple classification models using various algorithms.
+Compare the performance of these models and select the best one.
+Fulfill the process, performance, and deliverable requirements outlined in the project guidelines.
+Process Requirements
+Utilize any preprocessing methods and classification algorithms to find a model with high performance.
+Use R for building and testing classification models.
+Employ at least three attribute selection methods and two methods to create balanced datasets.
+Implement at least six different classification algorithms.
+Performance Requirements
+Refer to the "Grading Guideline" section provided in the project guidelines document.
+
+Deliverable Requirements
+Refer to the "Final Report" section provided in the project guidelines document.
+
+Directory Structure
+data: Contains the dataset file 'project_dataset-5K.csv' and any other related data files.
+preprocessing: Includes scripts or notebooks for data preprocessing.
+models: Contains scripts or notebooks for building and testing classification models.
+evaluation: Includes scripts or notebooks for evaluating model performance.
+reports: Contains the final project report, including analysis, results, and conclusions.
+Getting Started
+Clone this repository to your local machine.
+Navigate to the project directory.
+Follow the instructions provided in each directory to perform data preprocessing, model building, evaluation, and generating reports.
+Contributors
+Chuong Nguyen
+Brandon Bartol
